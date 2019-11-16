15d27f58149afff4734e458470ebc48d4d92138c
SandyDong <qwd1994@126.com>
2019-11-16 15:48:22 +0800

    新增排序算法测试类

---
+++ math-learn/src/main/java/com/dong/learn/Sort.java
@@ -0,0 +1,18 @@
+package com.dong.learn;
+
+/**
+ * 排序算法
+ */
+public class Sort {
+
+
+    /**
+     * 测试运行方法
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
