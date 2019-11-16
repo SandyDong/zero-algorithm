add80af153916e4c4ef08933dec0afe1c12dc0b5
SandyDong <qwd1994@126.com>
2019-11-16 15:23:31 +0800

    提交文件

---
+++ math-learn/src/main/java/com/dong/learn/MathLearn.java
@@ -0,0 +1,11 @@
+package com.dong.learn;
+
+public class MathLearn {
+
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("****************");
+    }
+
+}
