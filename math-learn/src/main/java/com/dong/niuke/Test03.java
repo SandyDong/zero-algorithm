63f1a6f14c19bee920f86f074ae7594a589f2e8b
DJ027972 <dwq940721>
2020-10-12 18:14:17 +0800

    提交代码

--- math-learn/src/main/java/com/dong/niuke/Test03.java
+++ math-learn/src/main/java/com/dong/niuke/Test03.java
@@ -8,30 +8,32 @@ import java.util.Scanner;
  * 输入描述：第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
  * <p>
  * 输出描述：输出输入字符串中含有该字符的个数。
+ * <p>
+ * 代码已通过案例测试
  */
 public class Test03 {
 
-    public static int getCountNum(String data1, char data2) {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        //输入参数一 忽略大小写
+        String data1 = sc.nextLine().toLowerCase();
+        //输入参数二 获取输入的单个字符
+        char charData = sc.nextLine().charAt(0);
+
+        Character.toLowerCase(charData);
+
         int countNum = 0;
+
         if (null != data1 && data1.length() > 0) {
             for (int i = 0; i < data1.length(); i++) {
-                if (data2 == data1.charAt(i)) {
+                if (charData == data1.charAt(i)) {
                     countNum++;
                 }
             }
-        }else{
+        } else {
             countNum = 0;
         }
-        return countNum;
-    }
-
-    public static void main(String[] args) {
-        Scanner scannerNum1 = new Scanner(System.in);
-        //输入参数一
-        String nextData1 = scannerNum1.next();
-        //输入参数二
-        char c = scannerNum1.next().charAt(0);
-        int countNum = getCountNum(nextData1, c);
         System.out.println(countNum);
     }
 }
